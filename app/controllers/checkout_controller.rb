d1a1ec9812e0f5af40d8e2133244fd552d70333a
callenprincipe <callenprincipe@yahoo.com>
2024-04-11 17:44:57 -0500

    Created feature to create orders

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -5,9 +5,34 @@ class CheckoutController < ApplicationController
   end
 
   def create_order
-    @customer = Customer.find_or_create_by(session[:customer_id])
-    @order = Order.create(customer_id: @customer.id, watch_id: session[:cart])
-    redirect_to confirmation_path
+    # Find or create the customer based on the session data
+    @customer = Customer.find_or_create_by(id: current_customer.id)
+
+    # Create a new order for the customer with today's date
+    @order = Order.new(customer_id: @customer.id, order_date: Time.zone.now)
+    Rails.logger.debug("Order: #{@order.id}")
+    # Save the order object
+    if @order.save
+      # Iterate over each item in the cart
+      session[:cart].each do |watch_id, quantity|
+        # Find the watch
+        watch = Watch.find(watch_id)
+
+        # Create an order detail for the watch and associate it with the order
+        @order.order_details.create(
+          order_id: @order.id,
+          watch_id: watch_id,
+          quantity: quantity,
+          price_at_order: watch.current_price
+        )
+      end
+
+      flash[:notice] = "Order ID: #{@order.id} has been successfully created."
+      redirect_back fallback_location: shopping_cart_index_path
+    else
+      # Handle the case where the order couldn't be saved
+      # This might include displaying an error message to the user
+    end
   end
 
   def calculate_total_price(cart_watches)
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get 'checkout', to: 'checkout#checkout', as: 'checkout'
 
+  post 'checkout/create_order', to: 'checkout#create_order', as: 'create_order_checkout'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   ActiveAdmin.routes(self)
